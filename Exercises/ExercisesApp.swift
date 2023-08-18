1f3973b92559c3d7c13194caff42347a8663de27
Alexander Neal Yagüe <alexander.nealyague@usp.ceu.es>
2023-08-18 16:58:56 +0200

    Initial Commit

---
+++ Exercises/ExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisesApp.swift
+//  Exercises
+//
+//  Created by Alexander Neal on 18/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
