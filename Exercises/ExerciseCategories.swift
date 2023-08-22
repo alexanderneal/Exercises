76dcae1622c2f2f01abeb502297cddac8120bd72
Alexander Neal Yagüe <alexander.nealyague@usp.ceu.es>
2023-08-22 19:35:37 +0200

    getting ready for legs

--- Exercises/ExerciseCategories.swift
+++ Exercises/ExerciseCategories.swift
@@ -15,6 +15,13 @@ extension Category {
             ]
         case .legs:
             return [
+                Exercise(id: 1, details: "1 set x 10 reps.", imageName: "plank"),
+                Exercise(id: 2, details: "1 set x 20 reps.", imageName: "abs_workout"),
+                Exercise(id: 3, details: "1 set x 15 reps.", imageName: "cable_down_abs"),
+                Exercise(id: 4, details: "2 set x 10 reps", imageName: "decline_abs"),
+                Exercise(id: 5, details: "1 set x 12 reps.", imageName: "hanging_leg_raise_ab_exercise"),
+                Exercise(id: 6, details: "1 set x 8 reps.", imageName: "rise_up_abs"),
+                Exercise(id: 7, details: "1 set x 22 reps.", imageName: "sat_down_skipping_abs"),
                 // ... Add exercises for legs category ...
             ]
         case .abs:
--- Exercises/ExerciseCell.swift
+++ Exercises/ExerciseCell.swift
@@ -4,7 +4,7 @@ struct ExerciseCell: View {
     var exercise: Exercise
     
     var body: some View {
-        HStack {
+        HStack { //Abs
             if exercise.imageName == "plank" {
                 Image("plank")
             } else if exercise.imageName == "cable_down_abs" {
@@ -21,8 +21,25 @@ struct ExerciseCell: View {
                 Image("sat_down_skipping_abs")
             } else if exercise.imageName == "side_plank" {
                 Image("side_plank")
+            //Legs
+            } else if exercise.imageName == "cable_down_abs" {
+                Image("cable_down_abs")
+            } else if exercise.imageName == "abs_workout" {
+                Image("abs_workout")
+            } else if exercise.imageName == "decline_abs" {
+                Image("decline_abs")
+            } else if exercise.imageName == "hanging_leg_raise_ab_exercise" {
+                Image("hanging_leg_raise_ab_exercise")
+            } else if exercise.imageName == "rise_up_abs" {
+                Image("rise_up_abs")
+            } else if exercise.imageName == "sat_down_skipping_abs" {
+                Image("sat_down_skipping_abs")
+            } else if exercise.imageName == "side_plank" {
+                Image("side_plank")
             }
 
+            
+            
             Text(exercise.details)
                 .foregroundColor(.black)
                 .padding(.horizontal)
