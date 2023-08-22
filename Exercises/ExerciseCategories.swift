0db39aaac3050a2e23ee3a687851b93447f85b69
Alexander Neal Yagüe <alexander.nealyague@usp.ceu.es>
2023-08-22 12:35:09 +0200

    Finalized Fully abs

--- Exercises/ExerciseCategories.swift
+++ Exercises/ExerciseCategories.swift
@@ -20,14 +20,14 @@ extension Category {
             ]
         case .abs:
             return [
-                Exercise(id: 1, details: "1 set x 20 reps.", imageName: "abs_workout"),
-                Exercise(id: 2, details: "1 set x 15 reps. ", imageName: "cable_down_abs"),
-                Exercise(id: 3, details: "1 set x 20 reps.", imageName: "decline_abs"),
-                Exercise(id: 4, details: "1 set x 10 reps.", imageName: "hanging_leg_raise_ab_exercise"),
-                Exercise(id: 5, details: "1 set x 1 min", imageName: "plank"),
-                Exercise(id: 6, details: "1 set x 15 reps.", imageName: "rise_up_abs"),
-                Exercise(id: 7, details: "1 set x 20 reps.", imageName: "sat_down_skipping_abs"),
-                Exercise(id: 8, details: "1 set x 30 secs ", imageName: "side_plank"),
+                Exercise(id: 1, details: "1 set x 1 min", imageName: "plank"),
+                Exercise(id: 2, details: "1 set x 20 reps.", imageName: "abs_workout"),
+                Exercise(id: 3, details: "1 set x 15 reps.", imageName: "cable_down_abs"),
+                Exercise(id: 4, details: "1 set x 12 reps.", imageName: "hanging_leg_raise_ab_exercise"),
+                Exercise(id: 5, details: "2 set x 10 reps", imageName: "decline_abs"),
+                Exercise(id: 6, details: "1 set x 8 reps.", imageName: "rise_up_abs"),
+                Exercise(id: 7, details: "1 set x 22 reps.", imageName: "sat_down_skipping_abs"),
+                Exercise(id: 8, details: "1 set x 30 secs", imageName: "side_plank"),
                 // Exercises for abs category
             ]
         }
--- Exercises/ExerciseCell.swift
+++ Exercises/ExerciseCell.swift
@@ -14,7 +14,7 @@ struct ExerciseCell: View {
                     .background(Color.gray)
                     .cornerRadius(10)
                     .padding(.leading, -40) // Add padding to the left
-            } else if exercise.details == "1 set x 15 reps. " {
+            } else if exercise.details == "1 set x 15 reps." {
                 Image("cable_down_abs")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -33,7 +33,7 @@ struct ExerciseCell: View {
                     .cornerRadius(10)
                     .padding(.leading, -40) // Add padding to the left
             }
-            else if exercise.details == "2 set x 10 reps " {
+            else if exercise.details == "2 set x 10 reps" {
                 Image("decline_abs")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
@@ -43,7 +43,46 @@ struct ExerciseCell: View {
                     .cornerRadius(10)
                     .padding(.leading, -40) // Add padding to the left
             }
-            // Add more conditions for other images
+            else if exercise.details == "1 set x 12 reps." {
+                Image("hanging_leg_raise_ab_exercise")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 130, height: 150)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .padding(.leading, -40) // Add padding to the left
+            }
+            else if exercise.details == "1 set x 8 reps." {
+                Image("rise_up_abs")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 130, height: 150)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .padding(.leading, -40) // Add padding to the left
+            }
+            else if exercise.details == "1 set x 22 reps." {
+                Image("sat_down_skipping_abs")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 130, height: 150)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .padding(.leading, -40) // Add padding to the left
+            }
+            else if exercise.details == "1 set x 30 secs" {
+                Image("side_plank")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 130, height: 150)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    .padding(.leading, -40) // Add padding to the left
+            }
             
             Text(exercise.details)
                 .foregroundColor(.black)
