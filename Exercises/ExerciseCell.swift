e5bf0b6a9b0d4554a687d1edb26f8d31a89a4989
Alexander Neal Yagüe <alexander.nealyague@usp.ceu.es>
2023-08-23 13:35:46 +0200

    legs done.

--- Exercises/ExerciseCell.swift
+++ Exercises/ExerciseCell.swift
@@ -22,20 +22,20 @@ struct ExerciseCell: View {
             } else if exercise.imageName == "side_plank" {
                 Image("side_plank")
             //Legs
-            } else if exercise.imageName == "cable_down_abs" {
-                Image("cable_down_abs")
-            } else if exercise.imageName == "abs_workout" {
-                Image("abs_workout")
-            } else if exercise.imageName == "decline_abs" {
-                Image("decline_abs")
-            } else if exercise.imageName == "hanging_leg_raise_ab_exercise" {
-                Image("hanging_leg_raise_ab_exercise")
-            } else if exercise.imageName == "rise_up_abs" {
-                Image("rise_up_abs")
-            } else if exercise.imageName == "sat_down_skipping_abs" {
-                Image("sat_down_skipping_abs")
-            } else if exercise.imageName == "side_plank" {
-                Image("side_plank")
+            } else if exercise.imageName == "squats" {
+                Image("squats")
+            } else if exercise.imageName == "leg_extension" {
+                Image("leg_extension")
+            } else if exercise.imageName == "leg_pressdown" {
+                Image("leg_pressdown")
+            } else if exercise.imageName == "steps_legs" {
+                Image("steps_legs")
+            } else if exercise.imageName == "down_press_legs" {
+                Image("down_press_legs")
+            } else if exercise.imageName == "pull_down_legs" {
+                Image("pull_down_legs")
+            } else if exercise.imageName == "leg_curls" {
+                Image("leg_curls")
             }
 
             
