46d8050e81f3055731947d1fac664f35198c4b19
Alexander Neal <alexander.nealyague@usp.ceu.es>
2025-08-15 17:41:26 +0200

    Changes of UI in Pilates module

--- Pilates/PilatesDetailView.swift
+++ Pilates/PilatesDetailView.swift
@@ -1,34 +1,25 @@
-//
-//  PilatesDetailView.swift
-//  Exercises
-//
-//  Created by Alexander Neal on 5/8/25.
-//
-
 import SwiftUI
+
 struct PilatesDetailView: View {
     
     let pilates: Pilates
     
     var body: some View {
-        VStack (alignment: .leading){
+            
             Image(pilates.imageName)
                 .resizable()
                 .scaledToFit()
-                .frame(height: 250)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .padding(.vertical, 10)
-
-            
-            
-            Text(pilates.imageName)
+                .frame(maxWidth: .infinity)
+                .ignoresSafeArea()
+        
+        VStack(alignment: .leading) {
+            Text(pilates.title)
                 .font(.title)
             
             HStack {
                 Text("Pilates")
-                    
                 Spacer()
-                Text(pilates.imageName)
+                Text(pilates.title)
             }
             .font(.subheadline)
             .foregroundStyle(.secondary)
@@ -42,14 +33,9 @@ struct PilatesDetailView: View {
             Text(pilates.description)
         }
         .padding()
-        
     }
-            
-            
-        }
-        
-    
-    #Preview {
-        PilatesDetailView(pilates: PilatesList.topPilates[0])
-    }
+}
 
+#Preview {
+    PilatesDetailView(pilates: PilatesList.topPilates[0])
+}
