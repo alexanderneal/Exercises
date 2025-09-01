91c9542634b7559f0aff2105cfa4b801e227e3b3
Alexander Neal <alexander.nealyague@usp.ceu.es>
2025-09-01 13:10:26 +0200

    additional changes of assets

--- Exercises/ExerciseCell.swift
+++ Exercises/ExerciseCell.swift
@@ -43,18 +43,32 @@ struct ExerciseCell: View {
             //Pecho
            else if exercise.imageName == "Bench press chest" {
                 Image("Bench press chest")
+                   .resizable()
+                   .frame(width: 150, height: 150)
             } else if exercise.imageName == "Chest dips pecho" {
                 Image("Chest dips pecho")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Forward chest" {
                 Image("Forward chest")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Incline dumbbells press" {
                 Image("Incline dumbbells press")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Open fly chest" {
                 Image("Open fly chest")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Opening chest" {
                 Image("Opening chest")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Push up chest" {
                 Image("Push up chest")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             }
             
             //Back
@@ -95,18 +109,32 @@ struct ExerciseCell: View {
             //Legs
              else if exercise.imageName == "Squats" {
                 Image("Squats")
+                     .resizable()
+                     .frame(width: 150, height: 150)
             } else if exercise.imageName == "Leg extension" {
                 Image("Leg extension")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Leg pressdown" {
                 Image("Leg pressdown")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Steps legs" {
                 Image("Steps legs")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Down press legs" {
                 Image("Down press legs")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Pull down legs" {
                 Image("Pull down legs")
+                    .resizable()
+                    .frame(width: 150, height: 150)
             } else if exercise.imageName == "Leg curls" {
                 Image("Leg curls")
+                    .resizable()
+                    .frame(width: 150, height: 150)
                 
             //Abs
             }
