b6ec5b1737bfca4522a9380b65fc44acf6233e4c
Alexander Neal <alexander.nealyague@usp.ceu.es>
2026-01-17 18:17:20 +0100

    UI changes in exercise view

--- Exercises/ExerciseDetail.swift
+++ Exercises/ExerciseDetail.swift
@@ -1,7 +1,5 @@
 import SwiftUI
 
-
-
 struct ExerciseDetail: View {
     @Environment(\.dismiss) private var dismiss
     @State private var showingSheet: Bool = false
@@ -9,44 +7,50 @@ struct ExerciseDetail: View {
     var exercise: Exercise
     
     var body: some View {
-        
-        VStack {
-            
+
             Image(exercise.imageName)
                 .resizable()
                 .scaledToFill()
                 .frame(maxWidth: .infinity)
                 .ignoresSafeArea(edges: .top)
+                .padding(.bottom)
+                .padding(.bottom)
             
-            
-            
-            Spacer()
+        VStack {
+            Text(exercise.imageName)
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
             
             HStack {
                 Text("Exercise")
                     .padding(.leading)
+                Spacer()
                 
                 Button(action: { showingSheet = true }) {
                     Label("", systemImage: "questionmark.circle")
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
+                Text(exercise.details)
+                    .padding(.trailing)
             }
             .font(.subheadline)
             .foregroundStyle(.secondary)
             
             Divider()
             
-                Text("About:")
-                    .frame(alignment: .leading)
-                    .font(.title2)
-                    .padding(.top)
-                    .padding(.horizontal)
-                    .frame(maxWidth: .infinity, alignment: .topLeading)
-                
-                Text(exercise.description)
-                    .padding(.horizontal)
-            }
+            Text("About")
+                .frame(alignment: .leading)
+                .font(.title2)
+                .padding(.top)
+                .padding(.horizontal)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+            
+            Text(exercise.description)
+                .padding(.horizontal)
+        
+        }
         .sheet(isPresented: $showingSheet) {
             HelpSheet(exercise: exercise)
                 .presentationDetents([.medium, .large])
@@ -88,6 +92,6 @@ struct HelpSheet: View {
 }
 
 #Preview {
-    ExerciseDetail(exercise: Exercise(id: 2, details: "1 set x 10 reps.", imageName: "Abs workout",description: "Keeping your back straight, and elbows close to your body, row the barbell towards your chest, squeezing your back muscles. Slowly lower to the starting position"))
+    ExerciseDetail(exercise: Exercise(id: 2, details: "1 set x 10 reps", imageName: "Abs workout",description: "Keeping your back straight, and elbows close to your body, row the barbell towards your chest, squeezing your back muscles. Slowly lower to the starting position"))
 }
 
